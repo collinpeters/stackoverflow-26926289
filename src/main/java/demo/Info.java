ac6e9d80b3bdb8573ce97cd7d57e0d3a6bca4f83
Collin Peters <cpeters@intouchfollowup.com>
2015-07-02 08:55:10 -0700

    Change to use Java 8 time classes

--- src/main/java/demo/Info.java
+++ src/main/java/demo/Info.java
@@ -1,19 +1,43 @@
 package demo;
 
-import org.joda.time.DateTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
-class Info{
+class Info {
 
-   private DateTime dateTime;
+	private LocalDate localDate;
+	private LocalDateTime localDateTime;
+	private ZonedDateTime zonedDateTime;
 
-    public Info(){
-        dateTime = new DateTime();
-    }
-    public DateTime getDateTime() {
-       return dateTime;
-    }
+	public Info() {
+		localDate = LocalDate.now();
+		localDateTime = LocalDateTime.now();
+		zonedDateTime = ZonedDateTime.now();
+	}
 
-    public void setDateTime(DateTime dateTime) {
-       this.dateTime = dateTime;
-    }
+	public LocalDate getLocalDate() {
+		return localDate;
+	}
+
+	public void setLocalDate(LocalDate localDate) {
+		this.localDate = localDate;
+	}
+
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
+
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+
+	public ZonedDateTime getZonedDateTime() {
+		return zonedDateTime;
+	}
+
+	public void setZonedDateTime(ZonedDateTime zonedDateTime) {
+		this.zonedDateTime = zonedDateTime;
+	}
+
 }
